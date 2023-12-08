99d4a4321f9ead25d9e16bc5c96295d2f560f650
feadre <zkbutt@qq.com>
2023-12-08 23:03:15 +0800

    前端CTR初步

--- app/src/main/java/top/feadre/faictr/activitys/CtrActivity.java
+++ app/src/main/java/top/feadre/faictr/activitys/CtrActivity.java
@@ -18,11 +18,56 @@
 package top.feadre.faictr.activitys;
 
 import android.os.Bundle;
+import android.view.View;
 
+import com.github.clans.fab.FloatingActionButton;
+import com.github.clans.fab.FloatingActionMenu;
+import com.xuexiang.xui.utils.XToastUtils;
+
+import butterknife.BindView;
+import rjsv.floatingmenu.floatingmenubutton.FloatingMenuButton;
+import rjsv.floatingmenu.floatingmenubutton.subbutton.FloatingSubButton;
 import top.feadre.faictr.R;
 import top.feadre.faictr.flib.base.FBaseActivity;
 
-public class CtrActivity extends FBaseActivity {
+public class CtrActivity extends FBaseActivity implements View.OnClickListener {
+    @BindView(R.id.fmb_ctr_l_bt1)
+    FloatingSubButton fmb_ctr_l_bt1;
+    @BindView(R.id.fmb_ctr_l_bt2)
+    FloatingSubButton fmb_ctr_l_bt2;
+    @BindView(R.id.fmb_ctr_l_bt3)
+    FloatingSubButton fmb_ctr_l_bt3;
+    @BindView(R.id.fmb_ctr_l_bt4)
+    FloatingSubButton fmb_ctr_l_bt4;
+    @BindView(R.id.fmb_ctr_l_bt5)
+    FloatingSubButton fmb_ctr_l_bt5;
+
+    @BindView(R.id.fmb_ctr_r_bt1)
+    FloatingSubButton fmb_ctr_r_bt1;
+    @BindView(R.id.fmb_ctr_r_bt2)
+    FloatingSubButton fmb_ctr_r_bt2;
+    @BindView(R.id.fmb_ctr_r_bt3)
+    FloatingSubButton fmb_ctr_r_bt3;
+    @BindView(R.id.fmb_ctr_r_bt4)
+    FloatingSubButton fmb_ctr_r_bt4;
+    @BindView(R.id.fmb_ctr_r_bt5)
+    FloatingSubButton fmb_ctr_r_bt5;
+
+    @BindView(R.id.fam_ctr_bt1)
+    FloatingActionButton fam_ctr_bt1;
+    @BindView(R.id.fam_ctr_bt2)
+    FloatingActionButton fam_ctr_bt2;
+    @BindView(R.id.fam_ctr_bt3)
+    FloatingActionButton fam_ctr_bt3;
+    @BindView(R.id.fam_ctr_bt4)
+    FloatingActionButton fam_ctr_bt4;
+    @BindView(R.id.fam_ctr_bt5)
+    FloatingActionButton fam_ctr_bt5;
+
+    private FloatingActionMenu fam_ctr;
+    private FloatingMenuButton fmb_ctr_r;
+    private FloatingMenuButton fmb_ctr_l;
+
     @Override
     protected int getLayoutId() {
         return R.layout.activity_ctr;
@@ -31,6 +76,85 @@ public class CtrActivity extends FBaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        fam_ctr = findViewById(R.id.fam_ctr);
+        fmb_ctr_r = findViewById(R.id.fmb_ctr_r);
+        fmb_ctr_l = findViewById(R.id.fmb_ctr_l);
 
+        fmb_ctr_l_bt5.setOnClickListener(this);
+        fmb_ctr_l_bt4.setOnClickListener(this);
+        fmb_ctr_l_bt3.setOnClickListener(this);
+        fmb_ctr_l_bt2.setOnClickListener(this);
+        fmb_ctr_l_bt1.setOnClickListener(this);
+
+        fmb_ctr_r_bt5.setOnClickListener(this);
+        fmb_ctr_r_bt4.setOnClickListener(this);
+        fmb_ctr_r_bt3.setOnClickListener(this);
+        fmb_ctr_r_bt2.setOnClickListener(this);
+        fmb_ctr_r_bt1.setOnClickListener(this);
+
+        fam_ctr_bt1.setOnClickListener(this);
+        fam_ctr_bt2.setOnClickListener(this);
+        fam_ctr_bt3.setOnClickListener(this);
+        fam_ctr_bt4.setOnClickListener(this);
+        fam_ctr_bt5.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+//        fam_ctr.getBackground().setAlpha(100);
+//        fmb_ctr_l.getBackground().setAlpha(20);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.fmb_ctr_l_bt1:
+                XToastUtils.info("fmb_ctr_l_bt1");
+                break;
+            case R.id.fmb_ctr_l_bt2:
+                XToastUtils.info("fmb_ctr_l_bt2");
+                break;
+            case R.id.fmb_ctr_l_bt3:
+                XToastUtils.info("fmb_ctr_l_bt3");
+                break;
+            case R.id.fmb_ctr_l_bt4:
+                XToastUtils.info("fmb_ctr_l_bt4");
+                break;
+            case R.id.fmb_ctr_l_bt5:
+                XToastUtils.info("fmb_ctr_l_bt5");
+                break;
+            case R.id.fmb_ctr_r_bt1:
+                XToastUtils.info("fmb_ctr_r_bt1");
+                break;
+            case R.id.fmb_ctr_r_bt2:
+                XToastUtils.info("fmb_ctr_r_bt2");
+                break;
+            case R.id.fmb_ctr_r_bt3:
+                XToastUtils.info("fmb_ctr_r_bt3");
+                break;
+            case R.id.fmb_ctr_r_bt4:
+                XToastUtils.info("fmb_ctr_r_bt4");
+                break;
+            case R.id.fmb_ctr_r_bt5:
+                XToastUtils.info("fmb_ctr_r_bt5");
+                break;
+            case R.id.fam_ctr_bt1:
+                XToastUtils.info("fam_ctr_bt1");
+                break;
+            case R.id.fam_ctr_bt2:
+                XToastUtils.info("fam_ctr_bt2");
+                break;
+            case R.id.fam_ctr_bt3:
+                XToastUtils.info("fam_ctr_bt3");
+                break;
+            case R.id.fam_ctr_bt4:
+                XToastUtils.info("fam_ctr_bt4");
+                break;
+            case R.id.fam_ctr_bt5:
+                XToastUtils.info("fam_ctr_bt5");
+                break;
+
+        }
     }
 }
